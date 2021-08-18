1f5df7bf462f7c107e09bfcebbcb668882fbe82d
mlevin <mlevin@CEGE-LEVIN.ad.umn.edu>
2021-08-18 12:47:30 -0500

    Bus stability detection work

--- src/mpsavs/MetaSimulation.java
+++ src/mpsavs/MetaSimulation.java
@@ -16,7 +16,7 @@ import java.util.Random;
  */
 public class MetaSimulation 
 {
-    public static double DETECT_EPSILON = 0.01;
+    public static double DETECT_EPSILON = 0.1;
     
     
     private int fleetsize;
